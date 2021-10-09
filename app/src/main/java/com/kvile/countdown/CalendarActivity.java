3099531d4a008cd67c21601aa4abfc66a7c8e5d2
Andreas Kvåle <andreas.kvale@tietoevry.com>
2021-10-09 20:40:37 +0200

    cleanup code and bump version

---
+++ app/src/main/java/com/kvile/countdown/CalendarActivity.java
@@ -0,0 +1,52 @@
+package com.kvile.countdown;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Calendar;
+
+public class CalendarActivity extends MainActivity {
+
+    private final static String UNNAMED_COUNTDOWN = "Unnamed";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        int editPosition = intent.getIntExtra(EDIT_COUNTDOWN_POSITION, -1);
+        setContentView(R.layout.activity_calendar);
+        EditText editText = findViewById(R.id.countdownName);
+        DatePicker datePicker = findViewById(R.id.datePicker);
+        datePicker.setMinDate(System.currentTimeMillis() - 1000);
+        if (editPosition >= 0) {
+            Countdown countdownToEdit = adapter.getCountdowns().get(editPosition);
+            editText.setText(countdownToEdit.getName());
+            Calendar calendar = countdownToEdit.getCalendar();
+            datePicker.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        }
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(v -> {
+            Calendar cal = Calendar.getInstance();
+            cal.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(), 0, 0, 0);
+            String name = StringUtils.isEmpty(editText.getText()) ? UNNAMED_COUNTDOWN : editText.getText().toString();
+            save(name, cal, editPosition);
+        });
+    }
+
+    protected void save(String name, Calendar cal, int editPosition) {
+        if (editPosition >= 0) {
+            adapter.editCountdown(new Countdown(name, cal), editPosition);
+        } else {
+            adapter.addCountdown(new Countdown(name, cal));
+        }
+        setList(STORE_FILE_NAME, adapter.getCountdowns());
+        Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+        CalendarActivity.this.startActivity(intent);
+    }
+
+}
